ac669a3f3737171eef33820b5394ad30a0cee3c2
Ndeva <ndeva3199@gmail.com>
2021-05-15 23:02:54 +0700

    cm

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,30 +35,12 @@ const loginController = async(req, res) => {
 }
 
 const regisController = async(req, res) => {
-    const { username, password } = req.body
-    let body = {}
-    for (const key in req.body) {
-        if (req.body[key]) {
-            body[key] = req.body[key]
-        }
-    }
-    if (!username || !password) return res.status(400).json({ "messenge": "vui long nhap thong tin" })
-    const data = await User.findOne({ 'username': username })
-    if (data) {
-        res.status(400).json({ resultCode: -1, "messenge": "User ton tai" })
-    } else {
-        bcrypt.hash(password, saltRounds).then(hash => {
-            body[password] = hash
-            body[Role] = "2"
-            var user = new User(body)
-            console.log(55555, user)
-            user.save(function(err, user) {
-                if (err) return console.error(err);
-                console.log(user)
-                res.status(200).json({ resultCode: 1, "messenge": "register thành công" }, )
-            });
-        })
-    }
+    const { username, password, displayName } = req.body
+    if (!username || !password) return res.status(400).json({ resultCode: -1, "messenge": "vui long nhap thong tin" })
+    return User.find({username}).then(user => {
+        if(user) return res.status(400).json({ resultCode: -1, "messenge": "vui long nhap thong tin" })
+        return User.create({username, password, displayName}).then(newUser => res.status(200).json({ resultCode: 1, "messenge": "Dang ky thanh cong" }))
+    })
 }
 
 const updateProfileController = async(req, res) => {
