4b3bb8687a42f65f768322afa437f64a8fd95d2b
Ndeva <ndeva3199@gmail.com>
2021-05-20 16:36:32 +0700

    cm

--- web/config/config.js
+++ web/config/config.js
@@ -1,5 +1,5 @@
 export const dev = process.env.NODE_ENV !== 'production'
 export const URL_DEV = 'http://localhost:5000/'
-export const URL_PRODUCTION = 'http://61.28.229.242/tda/'
+export const URL_PRODUCTION = 'https://rocky-island-31211.herokuapp.com/'
 export const URL = (dev ? URL_DEV : URL_PRODUCTION)
 export const API_URL = URL + 'api/'
