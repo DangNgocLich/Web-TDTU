88df6b13c851f964477b32de7584cb1973cb62a0
Ndeva <ndeva3199@gmail.com>
2021-05-20 16:56:12 +0700

    cm

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'googleAuth': {
         'clientID': '778577184334-ufmt60t13ahc4j2dhbr282fu62h8avfr.apps.googleusercontent.com',
         'clientSecret': '4QALRXjuUGS2LB7QfAkpXTZB',
-        'callbackURL': 'http://localhost:5000/api/auth/google/callback'
+        'callbackURL': process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api/auth/google/callback' : 'https://rocky-island-31211.herokuapp.com/api/auth/google/callback'
     }
 };
